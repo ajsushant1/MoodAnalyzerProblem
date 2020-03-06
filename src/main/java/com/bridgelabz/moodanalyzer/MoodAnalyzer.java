e1af5a23f90edf301244f8e345e851da4c517147
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-06 11:16:31 +0530

    ADDED: Welcome Message

---
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.moodanalyzer;
+
+public class MoodAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("/**************************/ WELCOME TO MOOD ANALYZER /**************************/");
+    }
+}
