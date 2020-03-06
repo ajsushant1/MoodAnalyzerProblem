e6d18497ee18695a5f1d7d154ccd02c07606f56b
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-06 12:07:43 +0530

    ADDED: Method to analyse mood

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,6 +1,16 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
+    //METHOD TO ANALYSE MOOD
+    public String analyseMood(String message) {
+        if (message.contains("Sad")){
+            return "SAD";
+        }
+        else {
+            return "HAPPY";
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO MOOD ANALYZER /**************************/");
     }
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,3 +1,19 @@
-public class MoodAnalyzerTest {
+import com.bridgelabz.moodanalyzer.MoodAnalyzer;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MoodAnalyzerTest {
+    MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
+    //TEST CASE TO ANALYSE SAD MOOD
+    @Test
+    public void givenMessage_WhenMoodSad_ShouldReturnSAD() {
+        String mood = moodAnalyzer.analyseMood("I am in Sad Mood");
+        Assert.assertEquals("SAD",mood);
+    }
+    //TEST CASE TO ANALYSE HAPPY OR ANY MOOD
+    @Test
+    public void givenMessage_WhenMoodHappyOrAny_ShouldReturnHAPPY() {
+        String mood = moodAnalyzer.analyseMood("I am in Any Mood");
+        Assert.assertEquals("HAPPY",mood);
+    }
 }
